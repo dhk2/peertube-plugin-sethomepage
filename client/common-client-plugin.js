6a2ea479d6bf798e8ef4092ae9939d2382499a87
dhk2 <donkimberlin@hotmail.com>
2021-05-02 22:47:26 -0700

    8

--- client/common-client-plugin.js
+++ client/common-client-plugin.js
@@ -2,19 +2,18 @@ function register ({ registerHook, peertubeHelpers }) {
   registerHook({
     target: 'action:router.navigation-end',
     handler: async ({ path }) => {
-      const c = await peertubeHelpers.getSettings('custom_links_markdown')
       const panel = document.createElement('div');
       panel.setAttribute('class', 'custom-links');
       html="this is a test only a test";
       panel.innerHTML = "this is halloween "+html;
       setInterval(async function(){
-        if (document.querySelector('.top-menu .custom-links') === null && c['custom_links_markdown']) {
+        if (document.querySelector('.top-menu .custom-links') === null ) {
           const topMenu = document.querySelector('.top-menu');
           if (topMenu) {
             topMenu.appendChild(panel);
           }
         }
-        if (document.querySelector('.menu-wrapper .custom-links') === null && c['custom_links_markdown']) {
+        if (document.querySelector('.menu-wrapper .custom-links') === null ) {
           const mainContent = document.querySelector('.menu-wrapper');
           if (mainContent) {
             panel.classList.add('section')
